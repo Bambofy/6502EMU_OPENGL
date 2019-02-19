755b201f221606a163a11c10a989b407adebd553
Richard Bamford <rbamfordz@gmail.com>
2019-02-19 13:44:09 +0000

    Add project files.

---
+++ 6502EMU/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _6502EMU
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
